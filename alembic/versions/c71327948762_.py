67fbe61a82fa7cde1530178688d9e4326636615c
Maksim Tsykalenko <tsykalenkom@gmail.com>
2021-03-23 18:16:30 +0300

    V2

---
+++ alembic/versions/c71327948762_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c71327948762
+Revises: 
+Create Date: 2021-03-23 16:00:15.748383
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c71327948762'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
